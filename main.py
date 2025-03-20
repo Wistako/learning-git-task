27493c89988ef636997c2612ac3ad2f49667cff1
Wistak <piotr.slomczynski@erpatech.com.pl>
2025-03-20 22:11:11 +0100

    Add logic to print shop_list

--- main.py
+++ main.py
@@ -3,3 +3,11 @@ shop_list = {
     'warzywniak': ['marchew', 'seler', 'rukole'],
     'miesny': ['lopatka', 'kosci', 'udka']
 }
+
+prod_len = 0
+for shop in shop_list:
+    prod_len += len(shop_list[shop])
+    shop_name = shop[0].upper() + shop[1:]
+    products_from_shop = [product[0].upper() + product[1:] for product in shop_list[shop]]
+    print(f"Idę do {shop_name} i kupuję tam {products_from_shop}")
+print(f"W sumie kupuje {prod_len} produktów.")
